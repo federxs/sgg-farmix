dcc0d633679f955f5eabe14506d03e1fa75fd2ce
fedepra <fpradomacat@gmail.com>
2017-06-20 14:45:05 -0300

    router de registrar bovino

--- sgg-farmix-web/wwwroot/app/routes.js
+++ sgg-farmix-web/wwwroot/app/routes.js
@@ -6,31 +6,41 @@
         $urlRouterProvider.otherwise("/home");
 
         /// Default
-        $stateProvider.state('home', {
-            url: "/home",
-            templateUrl: "app/master.html",
-            data: {
-                pageTitle: "Farmix-Home",
-                bodyClass: "master"
-            }//,
-            //controller: "loginController",
-            //resolve: {
-            //    deps: ['$ocLazyLoad', function ($ocLazyLoad) {
-            //        return $ocLazyLoad.load([{
-            //            name: 'app',
-            //            insertBefore: '#ng_load_plugins_before',
-            //            files: [
-            //		 //'assets/pages/css/login.min.css',
-            //		 //'assets/global/plugins/jquery-validation/js/jquery.validate.min.js',
-            //		 //'assets/global/plugins/jquery-validation/js/additional-methods.min.js',
-            //		 //'assets/global/scripts/app.min.js',
-            //		 //'assets/pages/scripts/login.min.js',
-            //		 //'scripts/services/portal/login-service.js',
-            //		 //'scripts/controllers/portal/login-controller.js'
-            //            ]
-            //        }]);
-            //    }]
-            //}
-        });
+        $stateProvider
+            .state('home', {
+                url: '/home',
+                templateUrl: 'app/master.html',
+                data: {
+                    pageTitle: 'Farmix-Home',
+                    bodyClass: 'master'
+                }
+            })
+            .state('registrarBovino', {
+                url: '/registrarBovino',
+                templateUrl: 'app/cabeza-ganado/alta-cabeza-ganado/alta-cabeza-ganado.html',
+                data: {
+                    pageTitle: 'Farmix - Registrar Bovino'
+                }
+            });
     });
-})();
+});
+
+//,
+//controller: "loginController",
+//resolve: {
+//    deps: ['$ocLazyLoad', function ($ocLazyLoad) {
+//        return $ocLazyLoad.load([{
+//            name: 'app',
+//            insertBefore: '#ng_load_plugins_before',
+//            files: [
+//		 //'assets/pages/css/login.min.css',
+//		 //'assets/global/plugins/jquery-validation/js/jquery.validate.min.js',
+//		 //'assets/global/plugins/jquery-validation/js/additional-methods.min.js',
+//		 //'assets/global/scripts/app.min.js',
+//		 //'assets/pages/scripts/login.min.js',
+//		 //'scripts/services/portal/login-service.js',
+//		 //'scripts/controllers/portal/login-controller.js'
+//            ]
+//        }]);
+//    }]
+//}
