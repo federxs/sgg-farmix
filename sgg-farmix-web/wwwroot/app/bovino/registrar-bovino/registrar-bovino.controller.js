7609759e1c8a5fa143af7c86639aa968c9f25b29
MatiPavan22 <matipavan94@gmail.com>
2017-07-01 18:20:44 -0300

    arreglo de fechaNacimiento para registrar

--- sgg-farmix-web/wwwroot/app/bovino/registrar-bovino/registrar-bovino.controller.js
+++ sgg-farmix-web/wwwroot/app/bovino/registrar-bovino/registrar-bovino.controller.js
@@ -33,11 +33,24 @@
         };
         $scope.load();
 
-        function registrar() {            
+        function registrar() {
+            vm.bovino.fechaNacimiento = convertirFecha(vm.bovino.fechaNacimiento);
             vm.bovino.$save(function (data) {
 
             });
 
+        }
+
+        function convertirFecha(fecha) {
+            var dia, mes, año;
+            dia = fecha.getDate().toString();
+            if (dia.length === 1)
+                dia = '0' + dia;
+            mes = fecha.getMonth().toString();
+            if (mes.length === 1)
+                mes = '0' + mes;
+            año = fecha.getFullYear().toString();
+            return dia + '/' + mes + '/' + año;
         }
 
         function validar() {
