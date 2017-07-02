e215f069746488eb2c4c7f77fa4f86b32fb1991c
MatiPavan22 <matipavan94@gmail.com>
2017-07-02 18:03:28 -0300

    arreglo de la fecha de Nacimiento, cuando el nro de caravana ya existe

--- sgg-farmix-web/wwwroot/app/bovino/registrar-bovino/registrar-bovino.controller.js
+++ sgg-farmix-web/wwwroot/app/bovino/registrar-bovino/registrar-bovino.controller.js
@@ -49,8 +49,11 @@
                 vm.habilitar = false;
                 vm.btnVolver = "Volver";
             }, function (error) {
-                if (error.statusText === 'Bovino ya existe')
+                if (error.statusText === 'Bovino ya existe') {
                     toastr.warning('Ya existe un bovino con ese número de caravana', 'Advertencia');
+                    var fecha = vm.bovino.fechaNacimiento.split('/');
+                    vm.bovino.fechaNacimiento = new Date(fecha[2], fecha[1], fecha[0]);
+                }
                 else {
                     toastr.error('La operación no se pudo completar', 'Error');
                 }
