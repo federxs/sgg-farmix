e185c9f1512a6afacf4a256cec0d4f6786499a15
MatiPavan22 <matipavan94@gmail.com>
2017-07-06 20:00:21 -0300

    Creacion proyecto Ionic para la parte mobile de Farmix

---
+++ sgg-farmix-appMobile/www/js/app.js
@@ -0,0 +1,67 @@
+angular.module('starter', ['ionic'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+      if (cordova.platformId === "ios" && window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+      cordova.plugins.Keyboard.disableScroll(true);
+
+    }
+    if (window.StatusBar) {
+      // org.apache.cordova.statusbar required
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+.config(function($stateProvider, $urlRouterProvider) {
+  $stateProvider
+
+    .state('app', {
+    url: '/app',
+    abstract: true,
+    templateUrl: 'views/menu.html',
+    controller: 'AppCtrl'
+  })
+
+  .state('app.search', {
+    url: '/search',
+    views: {
+      'menuContent': {
+          templateUrl: 'views/search.html'
+      }
+    }
+  })
+
+  .state('app.browse', {
+      url: '/browse',
+      views: {
+        'menuContent': {
+            templateUrl: 'views/browse.html'
+        }
+      }
+    })
+    .state('app.playlists', {
+      url: '/playlists',
+      views: {
+        'menuContent': {
+            templateUrl: 'views/playlists.html',
+          controller: 'PlaylistsCtrl'
+        }
+      }
+    })
+
+  .state('app.single', {
+    url: '/playlists/:playlistId',
+    views: {
+      'menuContent': {
+          templateUrl: 'views/playlist.html',
+        controller: 'PlaylistCtrl'
+      }
+    }
+  });
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise('/app/playlists');
+});
---
+++ sgg-farmix-appMobile/www/js/services/portal-service.js
@@ -0,0 +1,32 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('starter').service('portalService', function () {//usuarioInfo) {
+        var portalService = {};
+
+        portalService.getUrlServer = function () {
+            return "http://localhost:2424/";
+            // return "http://190.104.236.39/websiteyo/";
+            // return "http://yo.sooft.com.ar/";
+        };
+
+        /*portal.getDefaultUsuarioImagen = function () {
+            return "../images/portal/default-user.png";
+        };
+
+        portal.getDefaultUsuarioRecursos = function () {
+            return "recursos.es-AR.json";
+        };
+
+        portal.getHeadersServer = function () {
+            var headers = { 'Authorization': usuarioInfo.getToken() };
+            return headers;
+        };
+        portal.getContentUndefined = function () {
+            var headers = { 'Authorization': usuarioInfo.getToken(), 'Content-Type': undefined };
+            return headers;
+        };*/
+
+        return portalService;
+    });
+})();
