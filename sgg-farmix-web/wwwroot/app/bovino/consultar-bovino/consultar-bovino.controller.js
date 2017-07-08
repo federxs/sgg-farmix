459cd10832e50ac71937c4331910a4588ec1982d
MatiPavan22 <matipavan94@gmail.com>
2017-07-08 20:12:12 -0300

    agregue paginado a la tabla de consulta

--- sgg-farmix-web/wwwroot/app/bovino/consultar-bovino/consultar-bovino.controller.js
+++ sgg-farmix-web/wwwroot/app/bovino/consultar-bovino/consultar-bovino.controller.js
@@ -12,6 +12,9 @@
         //funciones
         vm.inicializar = inicializar();
         vm.consultar = consultar;
+        vm.paginar = paginar;
+        vm.anterior = anterior;
+        vm.siguiente = siguiente;
         //variables
         vm.razas = [];
         vm.estados = [];
@@ -20,7 +23,10 @@
         vm.establecimientos = [];
         vm.listaBovinos = [];
         vm.filtro = {};
-
+        vm.Paginas = [];
+        var registros = 5;
+        var bovinos = [];
+        var ultimoIndiceVisto = 0;
         function inicializar() {
             consultarBovinoService.inicializar({ idAmbitoEstado: '1' }, function (data) {
                 vm.estados = data.estados;
@@ -39,10 +45,61 @@
             //vm.bovino = new registrarBovinoService();
         };
 
+        function anterior() {
+            var paginaSelecciona = 0;
+            for (var i = 0; i < vm.Paginas.length; i++) {
+                if(vm.Paginas[i].seleccionada)
+                {
+                    paginaSelecciona = vm.Paginas[i - 1];
+                    break;
+                }
+            }
+            paginar(paginaSelecciona);
+        };
+
+        function siguiente() {
+            var paginaSelecciona = 0;
+            for (var i = 0; i < vm.Paginas.length; i++) {
+                if (vm.Paginas[i].seleccionada) {
+                    paginaSelecciona = vm.Paginas[i + 1];
+                    break;
+                }
+            }
+            paginar(paginaSelecciona);
+        };
+
+        function paginar(pag) {
+            for (var i = 0; i < vm.Paginas.length; i++) {
+                vm.Paginas[i].seleccionada = false;
+            };
+            pag.seleccionada = true;
+            vm.listaBovinos = [];
+            for (var i = pag.regInit; i < pag.regFin; i++) {
+                vm.listaBovinos.push(bovinos[i]);
+                if ((i + 1) === bovinos.length)
+                    break;
+            }
+        };
+
         function consultar() {
+            bovinos = [];
+            var cantPaginas = 0;
+            vm.Paginas = [];
+            vm.listaBovinos = [];
+            registros = 5;
             if (vm.filtro.peso === '') vm.filtro.peso = 0;
             consultarBovinoService.obtenerListaBovinos({ 'filtro': angular.toJson(vm.filtro, false) }, function (data) {
-                vm.listaBovinos = data;
+                bovinos = data;
+                cantPaginas = parseInt(data.length / registros);
+                for (var i = 0; i < (cantPaginas + 1) ; i++) {
+                    if (i === 0) vm.Paginas.push({ numPag: (i + 1), regInit: (registros * i), regFin: (registros * (i + 1)), seleccionada: true });
+                    else vm.Paginas.push({ numPag: (i + 1), regInit: (registros * i), regFin: (registros * (i + 1)), seleccionada: false });
+                }
+                if (data.length < registros) registros = data.length;
+                for (var i = 0; i < registros; i++) {
+                    vm.listaBovinos.push(data[i]);
+                }
+                //vm.listaBovinos = data;
                 vm.filtro.peso = '';
             }, function (error) {
                 toastr.error('Error: ' + error, 'Error');
