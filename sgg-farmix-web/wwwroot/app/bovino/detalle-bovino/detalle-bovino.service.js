f5171e9d71f1337a483f7b0a5005475f4c1c0f32
fedepra <fpradomacat@gmail.com>
2017-07-10 20:33:54 -0300

    validaciones pantalla eliminar, cambio nombres scripts, pantalla consultar rota

---
+++ sgg-farmix-web/wwwroot/app/bovino/detalle-bovino/detalle-bovino.service.js
@@ -0,0 +1,28 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('detalleBovinoService', detalleBovinoService);
+
+    detalleBovinoService.$inject = ['$http', 'portalService'];
+
+    function detalleBovinoService($http) {
+        var service = {
+            inicializar: inicializar,
+        };
+
+        function inicializar() {
+            return $http({
+                method: 'GET',
+                url: portalService.getUrlServer() + 'api/Bovino/initDetalle',
+                params: { idBovino: idBovino }
+            }).then(
+            function (data) {
+                return data.data || [];
+            });
+        }
+
+        return service;
+    }
+})();
