e7f59e6a603b365e1e95776b083337d7dd2da378
MatiPavan22 <matipavan94@gmail.com>
2017-08-22 20:51:05 -0300

    algo de vacuna para la app mobile

---
+++ sgg-farmix-acceso-datos/DAOs/VacunaManager.cs
@@ -0,0 +1,43 @@
+﻿using sgg_farmix_acceso_datos.Helper;
+using sgg_farmix_acceso_datos.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sgg_farmix_acceso_datos.DAOs
+{
+    public class VacunaManager : IManager<Vacuna>
+    {
+        public Vacuna Create(Vacuna entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Vacuna> Get(Vacuna entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vacuna Get(long id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vacuna GetFilter()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Vacuna Update(long id, Vacuna entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ sgg-farmix-acceso-datos/Model/Vacuna.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sgg_farmix_acceso_datos.Model
+{
+    public class Vacuna
+    {
+        public long idVacuna { get; set; }
+        public string nombre { get; set; }
+        public float cantidad { get; set; }
+    }
+}
