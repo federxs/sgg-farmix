adf0c5f0b5c3081a2189a63851e0be218fdde8c6
Fede Prado <fpradomacat@gmail.com>
2017-08-28 22:57:12 -0300

    se suma grafico de barras en el dashboard

--- sgg-farmix-web/wwwroot/app/inicio/inicio.controller.js
+++ sgg-farmix-web/wwwroot/app/inicio/inicio.controller.js
@@ -12,7 +12,13 @@
         $scope.myChartObject = {};
         $scope.myChartObject.type = "PieChart";
         $scope.myChartObject.options = {
-            'title': 'Proporción de vacas según la raza'
+            'width': 600,
+            'height': 400,
+            'chartArea': { 'width': '100%', 'height': '100%' },
+            'legend': {
+                'position': 'left',
+                'textStyle': { 'fontSize': 18 }
+            }
         };
         $scope.myChartObject.data = {
             "cols": [
@@ -51,5 +57,77 @@
                 }
             ]
         };
-    }
+
+        //grafico de barras de categorias
+        $scope.graficoCategorias = {};
+        $scope.graficoCategorias.type = "ColumnChart";
+        $scope.graficoCategorias.options = {
+            'theme': 'maximized',
+            //'width': 600,
+            'height': 400,
+            'chartArea': { 'left': 50, 'top': 30, 'right': 0, 'bottom':50},
+            'hAxis': { 'textPosition': 'out' }
+        };
+        $scope.graficoCategorias.data = {
+            "cols": [
+                { id: "t", label: "Categorías", type: "string" },
+                { id: "s", label: "Cantidad", type: "number" }
+            ], "rows": [
+                {
+                    c: [
+                       { v: "Ternera" },
+                       { v: 3 },
+                    ]
+                },
+                {
+                    c: [
+                       { v: "Vaquillona" },
+                       { v: 21 }
+                    ]
+                },
+                {
+                    c: [
+                       { v: "Vaca Joven" },
+                       { v: 31 }
+                    ]
+                },
+                {
+                    c: [
+                       { v: "Vaca Adulta" },
+                       { v: 31 }
+                    ]
+                },
+                {
+                    c: [
+                       { v: "Vaca Conserva" },
+                       { v: 5 },
+                    ]
+                },
+                {
+                    c: [
+                       { v: "Novillito" },
+                       { v: 2 },
+                    ]
+                },
+                {
+                    c: [
+                       { v: "Novillo" },
+                       { v: 6 },
+                    ]
+                },
+                {
+                    c: [
+                       { v: "Torito" },
+                       { v: 5 },
+                    ]
+                },
+                {
+                    c: [
+                       { v: "Toro" },
+                       { v: 10 },
+                    ]
+                }
+            ]
+        };
+    }//fin controlador
 })();
