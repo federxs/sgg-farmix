521ff29186cd70d30410a87b3eb89dd5a6ab39fc
Fede Prado <fpradomacat@gmail.com>
2017-09-07 19:39:46 -0300

    se suma el modulo inseminacion con la carpeta para consulta

---
+++ sgg-farmix-web/wwwroot/app/inseminacion/consultar-inseminacion/consultar-inseminacion.controller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('consultar_inseminacion', consultar_inseminacion);
+
+    consultar_inseminacion.$inject = ['$location']; 
+
+    function consultar_inseminacion($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'consultar_inseminacion';
+
+        activate();
+
+        function activate() { }
+    }
+})();
---
+++ sgg-farmix-web/wwwroot/app/inseminacion/consultar-inseminacion/consultar-inseminacion.service.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .factory('consultar_inseminacion', consultar_inseminacion);
+
+    consultar_inseminacion.$inject = ['$http'];
+
+    function consultar_inseminacion($http) {
+        var service = {
+            getData: getData
+        };
+
+        return service;
+
+        function getData() { }
+    }
+})();
