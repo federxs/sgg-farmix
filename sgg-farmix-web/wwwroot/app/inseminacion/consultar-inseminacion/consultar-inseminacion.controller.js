4f97cc0a41afec2cc9ccb5221c11d799b4713fcb
Fede Prado <fpradomacat@gmail.com>
2017-09-08 14:21:55 -0300

    se avanza con el template de la consulta de inseminacion

--- sgg-farmix-web/wwwroot/app/inseminacion/consultar-inseminacion/consultar-inseminacion.controller.js
+++ sgg-farmix-web/wwwroot/app/inseminacion/consultar-inseminacion/consultar-inseminacion.controller.js
@@ -10,6 +10,34 @@
     function consultarInseminacionController($scope, consultarInseminacionService) {
         var vm = $scope;
         //variables
+        vm.itemsPorPagina = 9;
+        //vm.rowCollection = [{col1: 'jajasd',col2: '123fda',col3: 'asdasd',col4: 'asdasd'},
+        //    {col1: 'asdas',col2: 'ddsad',col3: 'cxcvz',col4: 'zxczxc'},
+        //    {col1: 'czxcg',col2: 'fgdh',col3: '3242fsdf',col4: 'sdfsdfs'},
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' }];
+
+        //vm.caca = [{ col1: 'jajasd', col2: '123fda', col3: 'asdasd', col4: 'asdasd' },
+        //    { col1: 'asdas', col2: 'ddsad', col3: 'cxcvz', col4: 'zxczxc' },
+        //    { col1: 'czxcg', col2: 'fgdh', col3: '3242fsdf', col4: 'sdfsdfs' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' },
+        //    { col1: 'asdasf3', col2: '1243dfsdf', col3: '34234234', col4: 'asdasdasd' }];
         //metodos
         vm.inicializar = inicializar;
         inicializar();
