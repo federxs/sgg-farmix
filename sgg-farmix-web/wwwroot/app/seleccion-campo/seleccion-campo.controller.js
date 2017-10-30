6664255eadb2294d723825af89704a848824aef6
Fede Prado <fpradomacat@gmail.com>
2017-10-30 10:13:12 -0300

    pantalla seleccion de campo

--- sgg-farmix-web/wwwroot/app/seleccion-campo/seleccion-campo.controller.js
+++ sgg-farmix-web/wwwroot/app/seleccion-campo/seleccion-campo.controller.js
@@ -17,51 +17,15 @@
         vm.aceptar = aceptar;
 
         function inicializar() {
-            var obj = document.getElementById('btn_login');
-            obj.click();
-            if ($localStorage.usuarioInfo !== undefined) {
-                vm.usuario.usuario = $localStorage.usuarioInfo.usuario;
-                vm.ocultarUsuario = false;
-            }
+           
         }
 
         function aceptar() {
-            if (validar()) {
-                vm.showSpinner = true;
-                $scope.usuario.idRol = 1;
-                loginService.consultar($scope.usuario)
-                    .then(function success(data) {
-                        if (data.resultado === 1) {
-                            if ($localStorage.usuarioInfo === undefined) {
-                                $localStorage.usuarioInfo = {};
-                                $localStorage.usuarioInfo.usuario = vm.usuario.usuario;
-                                $localStorage.usuarioInfo.idRol = vm.usuario.idRol;
-                                $localStorage.usuarioInfo.codigoCampo = 100;
-                            }
-                            $('#login-modal').modal('hide');
-                            $state.go('home');
-                        }
-                        else
-                            toastr.error("Los datos son inválidos. Por favor revíselos e intente nuevamente.")
-                        vm.showSpinner = false;
-                    },
-                    function error(error) {
-                        vm.showSpinner = false;
-                        toastr.error("Los datos son inválidos. Por favor revíselos e intente nuevamente.")
-                    });
-            }
+            
         }
 
         function validar() {
-            if (isUndefinedOrNull(vm.usuario.usuario)) {
-                toastr.info("El usuario se encuentra vacío");
-                return false;
-            }
-            if (isUndefinedOrNull(vm.usuario.pass)) {
-                toastr.info("La contraseña se encuentra vacía");
-                return false;
-            }
-            return true;
+            
         }
 
         function isUndefinedOrNull(val) {
