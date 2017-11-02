c0f39f76c74aece99f6cdf959889012bef986057
lucaslissandrello <lucaslissandrello@gmail.com>
2017-11-02 16:12:04 -0300

    Algunas validacioenes extras para el login

--- sgg-farmix-appMobile/www/js/loginController.js
+++ sgg-farmix-appMobile/www/js/loginController.js
@@ -6,19 +6,23 @@ angular.module('starter')
         $rootScope.logueado = false;
         $scope.loginData = {};        
         $scope.doLogin = function () {
-            showIonicLoading().then(validarLogin).then(function (_login) {
-                if (_login.resultado == "1") {
-                    $rootScope.logueado = true;
-                    $localStorage.usuario = $scope.loginData.usuario;
-                    $localStorage.pass = $scope.loginData.pass;
-                    $state.go('app.bienvenido');
-                }
-                else {
-                    $rootScope.logueado = false;
-                    alert("Usuario o contraseña incorrecto.");
-                }
-            }).then($ionicLoading.hide).catch($ionicLoading.hide);
-        }
+            if (($scope.loginData.usuario === undefined || $scope.loginData.usuario === "") || ($scope.loginData.pass === undefined || $scope.loginData.pass === "")) {
+                alert("El usuario y la contraseña no pueden estar vacios.");
+            } else {
+                showIonicLoading().then(validarLogin).then(function (_login) {
+                    if (_login.resultado == "1") {
+                        $rootScope.logueado = true;
+                        $localStorage.usuario = $scope.loginData.usuario;
+                        $localStorage.pass = $scope.loginData.pass;
+                        $state.go('app.bienvenido');
+                    }
+                    else {
+                        $rootScope.logueado = false;
+                        alert("Usuario o contraseña incorrecto.");
+                    }
+                }).then($ionicLoading.hide).catch($ionicLoading.hide);
+            }
+      }
         function validarLogin() {
             $scope.loginData.idRol = 3;
             return loginService.validarLogin($scope.loginData);
