bd8835eb00d9e5b1fbd282b923c906b56bb21d73
Fede Prado <fpradomacat@gmail.com>
2017-11-04 19:15:17 -0300

    correcciones pantalla registro usuario

--- sgg-farmix-web/wwwroot/app/usuarios/registrar-usuarios/registrar-usuarios.controller.js
+++ sgg-farmix-web/wwwroot/app/usuarios/registrar-usuarios/registrar-usuarios.controller.js
@@ -9,9 +9,23 @@
 
     function registrarUsuariosController($scope) {
         var vm = $scope;
+        //variables
+        vm.showSpinner = true;
+        vm.btnVolver = "Cancelar";
+        vm.habilitar = true;
 
-        activate();
 
-        function activate() { }
+        //metodos
+        vm.inicializar = inicializar;
+        vm.registrar = registrar;
+        inicializar();
+
+        function inicializar() {
+            vm.showSpinner = false;
+        }
+
+        function registrar() { }
+
+        function validar() { }
     }
 })();
