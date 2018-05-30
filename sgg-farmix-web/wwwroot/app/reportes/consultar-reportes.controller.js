f639f53a1a4e4a9c64a8f429e0c922616080cea8
Fede Prado <fpradomacat@gmail.com>
2018-05-30 00:30:51 -0300

    Se agrega pagina ppal reportes

---
+++ sgg-farmix-web/wwwroot/app/reportes/consultar-reportes.controller.js
@@ -0,0 +1,14 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('consultarReportesController', consultarReportesController);
+
+    consultarReportesController.$inject = ['$scope'];
+
+    function consultarReportesController($scope) {
+        var vm = $scope;
+        
+    }
+})();
---
+++ sgg-farmix-web/wwwroot/app/reportes/consultar-reportes.service.js
@@ -0,0 +1,13 @@
+﻿(function () {
+    angular.module('app')
+        .factory('consultarReportesService', function ($resource, portalService) {
+            return $resource(portalService.getUrlServer() + 'api/Inconsistencia', {}, {
+                obtenerReportes: {
+                    method: 'GET',
+                    url: portalService.getUrlServer() + 'api/Inconsistencia/GetList',
+                    headers: portalService.getHeadersServer(),
+                    isArray: true
+                }
+            });
+        });
+})();
