bfa5bd3da564c98fbfbabf8e6ef3627ad44d3a02
Fede Prado <fpradomacat@gmail.com>
2018-06-18 21:01:06 -0300

    se agrega estructura para una estadistica

---
+++ sgg-farmix-web/wwwroot/app/reportes/estadistica-bovino/estadistica-bovino.controller.js
@@ -0,0 +1,39 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('estadisticaBovinoController', estadisticaBovinoController);
+
+    estadisticaBovinoController.$inject = ['$scope', 'estadisticaBovinoService', '$localStorage'];
+
+    function estadisticaBovinoController($scope, estadisticaBovinoService, $localStorage) {
+        var vm = $scope;
+
+        //funciones
+        vm.inicializar = inicializar();
+
+
+        //variables
+        window.scrollTo(0, 0);
+        vm.disabledExportar = 'disabled';
+        vm.bovinos = [];
+        vm.itemsPorPagina = 50;
+
+
+        inicializar();
+
+        function inicializar() {
+            $scope.$parent.blockSpinner();
+            estadisticaBovinoService.inicializar({
+                codigoCampo: $localStorage.usuarioInfo.codigoCampo
+            }, function (data) {
+                vm.rowCollection = data;
+                $scope.$parent.unBlockSpinner();
+            }, function error(error) {
+                $scope.$parent.unBlockSpinner();
+                toastr.error('Ha ocurrido un error, reintentar', 'Error');
+            });
+        }
+    }
+})();
---
+++ sgg-farmix-web/wwwroot/app/reportes/estadistica-bovino/estadistica-bovino.service.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    angular.module('app')
+        .factory('estadisticaBovinoService', function ($resource, portalService) {
+            return $resource(portalService.getUrlServer() + 'api/Estadisticas/', {}, {
+                inicializar: {
+                    method: 'GET',
+                    url: portalService.getUrlServer() + 'api/Estadisticas/Bovinos',
+                    headers: portalService.getHeadersServer(),
+                    params: {
+                        codigoCampo: '@codigoCampo'
+                    },
+                    isArray: true
+                }
+            });
+        });
+})();
