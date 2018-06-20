864290b526bd8cb166bb41886fb991a3021c15e4
Jere <jeremiasordonez@gmail.com>
2018-06-20 18:05:16 -0300

    Cambio carga bd tablas parametricas

--- sgg-farmix-appMobile/platforms/android/assets/www/js/services/parametricasService.js
+++ sgg-farmix-appMobile/platforms/android/assets/www/js/services/parametricasService.js
@@ -9,16 +9,22 @@
                     categoria = respuesta.data.categorias[i];
                     sqlStatments.push(["INSERT OR REPLACE INTO Categoria(idCategoria, nombre, genero) VALUES(?, ?, ?)", [categoria.idCategoria, categoria.nombre, categoria.genero]]);
                 }
+                $q(function (resolve, reject) {
+                    $rootScope.db.sqlBatch(sqlStatments, resolve, reject);
+                });
 
                 for (var i = 0; i < respuesta.data.razas.length; i++) {
                     raza = respuesta.data.razas[i];
                     sqlStatments.push(["INSERT OR REPLACE INTO Raza(idRaza, nombre) VALUES(?, ?, ?)", [raza.idRaza, raza.nombre]]);
                 }
+                $q(function (resolve, reject) {
+                    $rootScope.db.sqlBatch(sqlStatments, resolve, reject);
+                });
                 for (var i = 0; i < respuesta.data.estados.length; i++) {
                     estado = respuesta.data.estados[i];
                     sqlStatments.push(["INSERT OR REPLACE INTO Estado(idEstado, nombre, descripcion) VALUES(?, ?, ?)", [estado.idEstado, estado.nombre, estado.descripcion]]);
                 }
-                return $q(function (resolve, reject) {
+                $q(function (resolve, reject) {
                     $rootScope.db.sqlBatch(sqlStatments, resolve, reject);
                 });
             })
