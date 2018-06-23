f79ec18cd92cc1736dce9bacffd3ea78939e7b0c
Fede Prado <fpradomacat@gmail.com>
2018-06-23 15:43:06 -0300

    Se agrega top 5 de alimentos en estadisticas bovino

--- sgg-farmix-web/wwwroot/app/reportes/estadistica-bovino/estadistica-bovino.controller.js
+++ sgg-farmix-web/wwwroot/app/reportes/estadistica-bovino/estadistica-bovino.controller.js
@@ -12,11 +12,14 @@
         vm.title = 'estadisticaBovinoController';
 
         vm.cargarGraficoPesoPorRazaYSexo = cargarGraficoPesoPorRazaYSexo();
+        vm.cargarGraficoBajasPorAnio = cargarGraficoBajasPorAnio();
 
         init();
 
         function init() {
             cargarGraficoPesoPorRazaYSexo();
+            cargarGraficoBajasPorAnio();
+            cargarTablaAlimentosMasConsumidos();
         }
 
         function cargarGraficoPesoPorRazaYSexo(datos) {
@@ -26,7 +29,7 @@
                 'Id2': 3
             },
             {
-                'N': 'rangus',
+                'N': 'Brangus',
                 'Id1': 2,
                 'Id2': 3
             },
@@ -59,6 +62,10 @@
                     },
                     vAxis: {
                         title: 'Razas'
+                    },
+                    'legend': {
+                        'position': 'bottom',
+                        'textStyle': { 'fontSize': 12 }
                     }
                 };
                 chart.draw(dataTable, options);
@@ -67,41 +74,97 @@
 
         function cargarGraficoBajasPorAnio(datos) {
             var datos = [{
-                'N': 'Herbert',
-                'Id1': 2,
-                'Id2': 3
+                'anio': '2015',
+                'muertes': 5,
+                'ventas': 3
             },
             {
-                'N': 'rangus',
-                'Id1': 2,
-                'Id2': 3
+                'anio': '2016',
+                'muertes': 8,
+                'ventas': 5
             },
             {
-                'N': 'Angus',
-                'Id1': 2,
-                'Id2': 3
+                'anio': '2017',
+                'muertes': 4,
+                'ventas': 1
+            },
+            {
+                'anio': '2018',
+                'muertes': 4,
+                'ventas': 7
             }];
 
             google.charts.load('current', { 'packages': ['corechart'] });
             google.charts.setOnLoadCallback(drawChart);
 
             function drawChart() {
-                var container = document.getElementById('graficoPesoPorRazaYSexo');
-                var chart = new google.visualization.BarChart(container);
+                var container = document.getElementById('graficoBajasPorAnio');
+                var chart = new google.visualization.ColumnChart(container);
                 var dataTable = new google.visualization.DataTable();
 
-                dataTable.addColumn({ type: 'string', id: 'N' });
-                dataTable.addColumn({ type: 'number', id: 'Id1' });
-                dataTable.addColumn({ type: 'number', id: 'Id2' });
+                dataTable.addColumn({ id: 'anio', label: 'Año', type: 'string' });
+                dataTable.addColumn({ id: 'muertes', label: 'Muertes', type: 'number' });
+                dataTable.addColumn({ id: 'ventas', label: 'Ventas', type: 'number' });
 
                 for (var i = 0; i < datos.length; i++) {
-                    dataTable.addRows([[datos[i].N, datos[i].Id1, datos[i].Id2]]);
+                    dataTable.addRows([[datos[i].anio, datos[i].muertes, datos[i].ventas]]);
                 }
 
                 var options = {
-                    'title': 'Peso promedio según raza y sexo'
+                    'title': 'Cantidad de bajas por año',
+                    hAxis: {
+                        title: 'Años'
+                    },
+                    vAxis: {
+                        title: 'Nº de bajas'
+                    },
+                    'legend': {
+                        'position': 'bottom',
+                        'textStyle': { 'fontSize': 12 }
+                    }
                 };
-                chart.draw(data, options);
+                chart.draw(dataTable, options);
+            }
+        };
+
+        function cargarTablaAlimentosMasConsumidos(datos) {
+            var datos = [{
+                'alimento': 'Caca de perro',
+                'cantidad': 104
+            },
+            {
+                'alimento': 'Caca de gallina',
+                'cantidad': 45
+            },
+            {
+                'alimento': 'Caca de lukini',
+                'cantidad': 20
+            },
+            {
+                'alimento': 'Caca de lurilu',
+                'cantidad': 5
+            },
+            {
+                'alimento': 'Caca de guere',
+                'cantidad': 1
+            }];
+
+            google.charts.load('current', { 'packages': ['table'] });
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+                var container = document.getElementById('tablaAlimentosMasConsumidos');
+                var table = new google.visualization.Table(container);
+                var dataTable = new google.visualization.DataTable();
+
+                dataTable.addColumn({ id: 'alimento', label: 'Alimento', type: 'string' });
+                dataTable.addColumn({ id: 'cantidad', label: 'Cantidad', type: 'number' });
+
+                for (var i = 0; i < datos.length; i++) {
+                    dataTable.addRows([[datos[i].alimento, datos[i].cantidad]]);
+                }
+
+                table.draw(dataTable, { showRowNumber: true, width: '100%', height: '100%' });
             }
         };
     }//fin controlador
