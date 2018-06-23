4fb99719b02ebf961013afcef2f059ba7ea5b3fe
Fede Prado <fpradomacat@gmail.com>
2018-06-23 14:27:13 -0300

    se agrega informacion  en los ejes de los graficos

--- sgg-farmix-web/wwwroot/app/reportes/estadistica-bovino/estadistica-bovino.controller.js
+++ sgg-farmix-web/wwwroot/app/reportes/estadistica-bovino/estadistica-bovino.controller.js
@@ -35,6 +35,53 @@
                 'Id1': 2,
                 'Id2': 3
             }];
+
+            google.charts.load('current', { 'packages': ['corechart'] });
+            google.charts.setOnLoadCallback(drawChart);
+
+            function drawChart() {
+                var container = document.getElementById('graficoPesoPorRazaYSexo');
+                var chart = new google.visualization.BarChart(container);
+                var dataTable = new google.visualization.DataTable();
+
+                dataTable.addColumn({ type: 'string', id: 'N' });
+                dataTable.addColumn({ type: 'number', id: 'Id1' });
+                dataTable.addColumn({ type: 'number', id: 'Id2' });
+
+                for (var i = 0; i < datos.length; i++) {
+                    dataTable.addRows([[datos[i].N, datos[i].Id1, datos[i].Id2]]);
+                }
+
+                var options = {
+                    'title': 'Peso promedio según raza y sexo',
+                    hAxis: {
+                        title: 'Peso en kg.'
+                    },
+                    vAxis: {
+                        title: 'Razas'
+                    }
+                };
+                chart.draw(dataTable, options);
+            }
+        };
+
+        function cargarGraficoBajasPorAnio(datos) {
+            var datos = [{
+                'N': 'Herbert',
+                'Id1': 2,
+                'Id2': 3
+            },
+            {
+                'N': 'rangus',
+                'Id1': 2,
+                'Id2': 3
+            },
+            {
+                'N': 'Angus',
+                'Id1': 2,
+                'Id2': 3
+            }];
+            
             google.charts.load('current', { 'packages': ['corechart'] });
             google.charts.setOnLoadCallback(drawChart);
 
@@ -54,8 +101,8 @@
                 var options = {
                     'title': 'Peso promedio según raza y sexo'
                 };
-                chart.draw(dataTable, options);
+                chart.draw(data, options);
             }
         };
-    }
+    }//fin controlador
 })();
