b128483e863c4b80f192e873b0077d7cde995b82
Fede Prado <fpradomacat@gmail.com>
2018-06-23 18:40:05 -0300

    cambios de orden de graficos y referncias en controlador de estadisticas bovino

--- sgg-farmix-web/wwwroot/app/reportes/estadistica-bovino/estadistica-bovino.controller.js
+++ sgg-farmix-web/wwwroot/app/reportes/estadistica-bovino/estadistica-bovino.controller.js
@@ -233,8 +233,8 @@
                 table.draw(dataTable, { showRowNumber: true, width: '100%', height: '100%' });
 
                 var my_anchor = document.getElementById('descargaTablaAlimentosMasConsumidos');
-                my_anchor.href = chart.getImageURI();
-                google.visualization.events.addListener(chart, 'ready', function () {
+                my_anchor.href = table.getImageURI();
+                google.visualization.events.addListener(table, 'ready', function () {
                     my_anchor.innerHTML = '<img src="' + table.getImageURI() + '">';
                 });
             }
