ca3d6dbbac5227f3605110ea0ef221bb9ecf1108
lucaslissandrello <lucaslissandrello@gmail.com>
2018-07-26 22:27:17 -0300

    Cambios en la parte Web de Eventos Reportes
    Se modifico los Reportes de Eventos, se le agrego los filtros pero solo se hizo la parte web (html y controller y el service). Falta la api y el sp

--- sgg-farmix-web/wwwroot/app/reportes/reporte-evento/reporte-evento.controller.js
+++ sgg-farmix-web/wwwroot/app/reportes/reporte-evento/reporte-evento.controller.js
@@ -9,24 +9,39 @@
 
     function reporteEventoController($scope, reporteEventoService, $localStorage, portalService, toastr, $state, $sessionStorage) {
         var vm = $scope;
-
+        window.scrollTo(0, 0);
+        $('.modal-backdrop').remove();
+        $('[data-toggle="tooltip"]').tooltip();
+        vm.disabled = 'disabled';
+        vm.disabledExportar = 'disabled';
+        vm.tipoEventoPopUp = '';
+        vm.fecha = '';
         //funciones
         vm.inicializar = inicializar();
-
-        //variables
-        window.scrollTo(0, 0);
-        vm.bovinos = [];
-        vm.itemsPorPagina = 50;
-
-        vm.exportarPDF = exportarPDF;
+        vm.consultar = consultar;
+        vm.limpiarCampos = limpiarCampos;
         vm.exportarExcel = exportarExcel;
+        vm.openPopUp = openPopUp;
+        vm.eliminar = eliminar;
+        vm.getFechaDesde = getFechaDesde;
+        vm.getFechaHasta = getFechaHasta;
+        vm.exportarPDF = exportarPDF;
+        //variables       
+        vm.filtro = {};
+        vm.cursor = '';
+        vm.numCaravanaFiltro;
+        var ultimoIndiceVisto = 0;
+        var idEventoAEliminar = 0;
+        var idManejo = [];
+        var idAlimenticio = [];
+        vm.fechaDeHoy = new Date();
+        $('#datetimepicker4').datetimepicker();
+        $('#datetimepicker5').datetimepicker();
+        vm.itemsPorPagina = 50;
 
         function inicializar() {
             $scope.$parent.blockSpinner();
-            reporteEventoService.inicializar({
-                codigoCampo: $localStorage.usuarioInfo.codigoCampo,
-                periodo: $localStorage.usuarioInfo.periodoConsulta
-            }, function (data) {
+            reporteEventoService.consultarEventos({ 'filtro': angular.toJson(vm.filtro, false) }, function (data) {
                 vm.rowCollection = data;
                 $scope.$parent.unBlockSpinner();
                 if (vm.rowCollection.length === 0) {
@@ -37,6 +52,83 @@
                 $scope.$parent.unBlockSpinner();
                 $scope.$parent.errorServicio(error.statusText);
             });
+        }
+
+        function consultar() {
+            $scope.$parent.blockSpinner();
+            vm.disabled = 'disabled';
+            vm.disabledExportar = 'disabled';
+            if (vm.filtro.fechaDesde !== undefined) {
+                if (typeof vm.filtro.fechaDesde === "string")
+                    vm.filtro.fechaDesde = new Date(vm.filtro.fechaDesde.split('/')[2], (parseInt(vm.filtro.fechaDesde.split('/')[1]) - 1).toString(), vm.filtro.fechaDesde.split('/')[0]);
+                vm.filtro.fechaDesde = convertirFecha(vm.filtro.fechaDesde);
+            }
+            if (vm.filtro.fechaHasta !== undefined) {
+                if (typeof vm.filtro.fechaHasta === "string")
+                    vm.filtro.fechaHasta = new Date(vm.filtro.fechaHasta.split('/')[2], (parseInt(vm.filtro.fechaHasta.split('/')[1]) - 1).toString(), vm.filtro.fechaHasta.split('/')[0]);
+                vm.filtro.fechaHasta = convertirFecha(vm.filtro.fechaHasta);
+            }
+            if (vm.filtro.numCaravana === null) {
+                vm.filtro.numCaravana = undefined;
+                $('#timeline').hide();
+            }
+            else if (vm.filtro.numCaravana !== undefined && vm.filtro.numCaravana !== null) {
+                $('#timeline').show();
+            }
+            vm.filtro.periodo = $localStorage.usuarioInfo.periodoConsulta;
+            reporteEventoService.consultarEventos({ 'filtro': angular.toJson(vm.filtro, false) }, function (data) {
+                vm.numCaravanaFiltro = undefined;
+                if (data.length === 0) {
+                    vm.disabledExportar = 'disabled';
+                    vm.disabled = '';
+                    vm.rowCollection = [];
+                    $('#timeline').hide();
+                    toastr.info("No se ha encontrado ningún resultado para esta búsqueda", "Aviso");
+                }
+                else {
+                    vm.rowCollection = data;
+                    if (vm.filtro.numCaravana !== undefined && vm.filtro.numCaravana !== null) {
+                        vm.numCaravanaFiltro = angular.copy(vm.filtro.numCaravana);
+                        cargarLineaTiempoEventos();
+                    }
+                    if (vm.filtro.numCaravana === 0) vm.filtro.numCaravana = '';
+                    vm.disabled = '';
+                    vm.disabledExportar = '';
+                }
+                $scope.$parent.unBlockSpinner();
+                $('.modal-backdrop').remove();
+            }, function error(error) {
+                $scope.$parent.unBlockSpinner();
+                $scope.$parent.errorServicio(error.statusText);
+            });
+        };
+
+        function ordenarFechasMenorAMayor(lista) {
+            var listaARetornar = [];
+            for (var i = lista.length - 1; i >= 0; i--) {
+                listaARetornar.push(lista[i]);
+            }
+
+        function limpiarCampos() {
+            $state.reload();
+        }
+
+        function convertirFecha(fecha) {
+            var dia, mes, año, hora, min;
+            dia = fecha.getDate().toString();
+            if (dia.length === 1)
+                dia = '0' + dia;
+            mes = (fecha.getMonth() + 1).toString();
+            if (mes.length === 1)
+                mes = '0' + mes;
+            año = fecha.getFullYear().toString();
+            hora = fecha.getHours().toString();
+            if (hora.length === 1)
+                hora = '0' + hora;
+            min = fecha.getMinutes().toString();
+            if (min.length === 1)
+                min = '0' + min;
+            return dia + '/' + mes + '/' + año + ' ' + hora + ':' + min;
         }
 
         function exportarPDF() {
--- sgg-farmix-web/wwwroot/app/reportes/reporte-evento/reporte-evento.service.js
+++ sgg-farmix-web/wwwroot/app/reportes/reporte-evento/reporte-evento.service.js
@@ -2,14 +2,10 @@
     angular.module('app')
         .factory('reporteEventoService', function ($resource, portalService) {
             return $resource(portalService.getUrlServer() + 'api/Reportes/', {}, {
-                inicializar: {
+                consultarEventos: {
                     method: 'GET',
                     url: portalService.getUrlServer() + 'api/Reportes/Eventos',
                     headers: portalService.getHeadersServer(),
-                    params: {
-                        codigoCampo: '@codigoCampo',
-                        periodo: '@periodo'
-                    },
                     isArray: true
                 },
                 generarPDF: {
